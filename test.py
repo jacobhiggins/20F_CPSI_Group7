2b966dc20190f681cb4b8dcffa4eadc58fb0b28c
ss8ry <71779322+ss8ry@users.noreply.github.com>
2020-09-27 11:44:37 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import influxdb
+
+HOST = 'influx.linklab.virginia.edu'
+PORT = 443
+USERNAME = 'cps1'
+PASSWORD = 'ea0quoko2oajae1NiakiethaihofoseiY'
+DATABASE = 'living-link-lab-open'
+
+client = influxdb.InfluxDBClient(HOST, PORT, USERNAME, PASSWORD, DATABASE, ssl=True, verify_ssl=True)
+
+result = client.query('SELECT value FROM voc_ppb LIMIT 1;')
+print("Result: {}".format(result))
