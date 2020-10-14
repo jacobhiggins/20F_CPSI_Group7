f7ca86d6bdcad43969457a64ebc29205d98183e5
mwjeong124 <71984455+mwjeong124@users.noreply.github.com>
2020-10-14 03:23:51 -0400

    Update freshest_room.py
    reduced code so first hour doesn't need to be done separately

--- freshest_room.py
+++ freshest_room.py
@@ -17,45 +17,32 @@ base_string = "SELECT MEAN(\"value\") FROM {} WHERE time > '{}T{}Z' AND time < '
 # Define dates
 date = ["2020-09-18", "2020-09-19", "2020-09-20"]
 
+# repeat for 3 different dates
 for r in range(len(date)):
-    # define dictionaries for mean values and max mean values for each room and hour
+    # define dictionaries for mean values and maximum mean values for each room and hour
     awair_mean_values = {}
     awair_max_values = {}
 
-    # define time range for first hour (only could do this process by doing first hour separately from the remaining 23 - probably not most efficient)
-    time_start_string = "{0:0>2d}:00:00".format(1)
-    time_end_string = "{0:0>2d}:59:59".format(1)
-
-    #  construct query string
-    query_string = base_string.format(Freshest,date[r],time_start_string,date[r],time_end_string)
-
-    # get mean values for each room at each hour
-    result = client.query(query_string)
-    result_list = result.raw["series"]
-
-    # create dictionary where keys are rooms and values are list of mean values
-    for j in range(len(result_list)):
-        awair_mean_values[result_list[j]["tags"]["location_specific"]] = []
-        awair_mean_values[result_list[j]["tags"]["location_specific"]].append(result_list[j]["values"][0][1])
-
-    # repeat process for remaining 23 hours
-    for i in range(23):
-        # define time range for remaining 23 hours
-        time_start_string = "{0:0>2d}:00:00".format(i+1)
-        time_end_string = "{0:0>2d}:59:59".format(i+1)
+    # repeat for each hour
+    for i in range(24):
+        # define time range for each hours
+        time_start_string = "{0:0>2d}:00:00".format(i)
+        time_end_string = "{0:0>2d}:59:59".format(i)
 
         # construct query string
         query_string = base_string.format(Freshest, date[r], time_start_string, date[r], time_end_string)
 
-        # mean values for each room for remaining 23 hours
+        # mean values for each room
         result = client.query(query_string)
         result_list = result.raw["series"]
 
-        # append dictionary to include all mean values for each hour
+        # append dictionary where dictionary keys are room and dictionary values are mean values
         for j in range(len(result_list)):
+            if i == 0 & j == 0:
+                awair_mean_values[result_list[j]["tags"]["location_specific"]] = []
             awair_mean_values[result_list[j]["tags"]["location_specific"]].append(result_list[j]["values"][0][1])
 
-    # append second dictionary (awair_max_values) to only include maximumm mean values
+    # append second dictionary (awair_max_values) to only include maximum mean awair values
     for x in awair_mean_values:
         awair_max_values[x] = max(awair_mean_values[x])
 
